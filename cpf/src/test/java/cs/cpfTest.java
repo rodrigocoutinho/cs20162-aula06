2182bbac7f62a46d2eb59a9696a3ae62e7e197e1
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2016-10-04 12:53:25 -0300

    Exercicios aula06

---
+++ cpf/src/test/java/cs/cpfTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Shakaw
+ */
+public class cpfTest {
+    
+    public cpfTest() {
+    }
+
+    /**
+     * Test of cpf method, of class cpf.
+     */
+    @Test
+    public void testCpf() {
+    }
+
+    /**
+     * Test of cpf2 method, of class cpf.
+     */
+    @Test
+    public void testCpf2() {
+    }
+
+    /**
+     * Test of mod method, of class cpf.
+     */
+    @Test
+    public void testMod() {
+    }
+    
+}
