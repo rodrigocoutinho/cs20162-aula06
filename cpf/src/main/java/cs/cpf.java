6b12f3a829c576bb182e2232fef5ce805200243b
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2016-10-22 21:17:37 -0200

    Correção de testes

--- cpf/src/main/java/cs/cpf.java
+++ cpf/src/main/java/cs/cpf.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
  */
 package cs;
 
--- cpf/src/test/java/cs/cpfTest.java
+++ cpf/src/test/java/cs/cpfTest.java
@@ -1,37 +1,43 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
  */
 package cs;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Shakaw
  */
 public class cpfTest {
-    
-    public cpfTest() {
-    }
 
     /**
      * Testa vetor com numeros válidos.
      */
     @Test
     public void testCpf() {
-        int n[] = {0,1,4,5,3,4,2,3,1,6,5};
+        int n[] = {0, 1, 4, 5, 3, 4, 2, 3, 1, 6, 5};
         Cpf.cpf(n);
     }
-    
+
     /**
      * Testa a entrada de numeros inválido.
+     *
+     * @throws java.lang.Exception
      */
     @Test(expected = IllegalArgumentException.class)
     public void testNumeroInvalido() throws Exception {
-        int n[] = {0,0,0,0,0,0,0,0,0,0,0};
+        int n[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Cpf.cpf(n);
+    }
+
+    /**
+     * Testa a entrada de numeros inválido.
+     *
+     */
+    public void testNumeroInvalido2() {
+        int n[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         Cpf.cpf(n);
     }
 
@@ -40,16 +46,27 @@ public class cpfTest {
      */
     @Test
     public void testCpf2() {
-        int n[] = {0,1,4,5,3,4,2,3,1,6,5};
+        int n[] = {0, 1, 4, 5, 3, 4, 2, 3, 1, 6, 5};
         Cpf.cpf2(n);
     }
-    
+
     /**
      * Testa a entrada de numeros inválido.
+     *
+     * @throws java.lang.Exception
      */
     @Test(expected = IllegalArgumentException.class)
-    public void testNumeroInvalido2() throws Exception {
-        int n[] = {0,0,0,0,0,0,0,0,0,0,0};
+    public void testNumeroInvalido3() throws Exception {
+        int n[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Cpf.cpf2(n);
+    }
+
+    /**
+     * Testa a entrada de numeros inválido.
+     *
+     */
+    public void testNumeroInvalido4() {
+        int n[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         Cpf.cpf2(n);
     }
 
@@ -58,7 +75,7 @@ public class cpfTest {
      */
     @Test
     public void testMod() {
-        Cpf.mod(3,2);
+        Cpf.mod(3, 2);
     }
-    
+
 }
